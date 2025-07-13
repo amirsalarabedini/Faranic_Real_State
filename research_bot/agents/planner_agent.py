9759d0d3a92475c8c7e07f99cb0507b11668371b
amirsalarabedini <abediniamirsalar@gmail.com>
2025-07-13 16:22:03 +0330

    meeeeooo

--- research_bot/agents/planner_agent.py
+++ research_bot/agents/planner_agent.py
@@ -1,11 +1,7 @@
 from pydantic import BaseModel
 
 from agents import Agent
-
-PROMPT = (
-    "You are a helpful research assistant. Given a query, come up with a set of web searches "
-    "to perform to best answer the query. Output between 5 and 20 terms to query for."
-)
+from .prompts.prompts import PLANNER_AGENT_PROMPT
 
 
 class WebSearchItem(BaseModel):
@@ -23,7 +19,7 @@ class WebSearchPlan(BaseModel):
 
 planner_agent = Agent(
     name="PlannerAgent",
-    instructions=PROMPT,
+    instructions=PLANNER_AGENT_PROMPT,
     model="gpt-4o",
     output_type=WebSearchPlan,
 )
--- research_bot/agents/writer_agent.py
+++ research_bot/agents/writer_agent.py
@@ -2,16 +2,7 @@
 from pydantic import BaseModel
 
 from agents import Agent
-
-PROMPT = (
-    "You are a senior researcher tasked with writing a cohesive report for a research query. "
-    "You will be provided with the original query, and some initial research done by a research "
-    "assistant.\n"
-    "You should first come up with an outline for the report that describes the structure and "
-    "flow of the report. Then, generate the report and return that as your final output.\n"
-    "The final output should be in markdown format, and it should be lengthy and detailed. Aim "
-    "for 5-10 pages of content, at least 1000 words."
-)
+from .prompts.prompts import WRITER_AGENT_PROMPT
 
 
 class ReportData(BaseModel):
@@ -27,7 +18,7 @@ class ReportData(BaseModel):
 
 writer_agent = Agent(
     name="WriterAgent",
-    instructions=PROMPT,
-    model="o3-mini",
+    instructions=WRITER_AGENT_PROMPT,
+    model="o4-mini",
     output_type=ReportData,
 )
